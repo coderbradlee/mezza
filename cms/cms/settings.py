5dea02bc254aca868b197401f222fef04131a6c5
lzxm160 <lzxm160@sina.com>
2017-01-14 10:55:42 +0800

    html5boilerplate

--- cms/cms/settings.py
+++ cms/cms/settings.py
@@ -261,7 +261,7 @@ INSTALLED_APPS = (
     # "mezzanine.accounts",
     # "mezzanine.mobile",
 )
-
+THEME="mezzanine_html5boilerplate"
 # List of middleware classes to use. Order is important; in the request phase,
 # these middleware classes will be applied in the order given, and in the
 # response phase the middleware will be applied in reverse order.
